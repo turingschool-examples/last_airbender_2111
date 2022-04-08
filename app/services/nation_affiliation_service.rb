55648987203973c6794a9f69c088beb266d5e185
Aedan Yturralde <aedanyturralde@gmail.com>
2022-04-08 11:00:25 -0600

    final commit

--- app/services/nation_affiliation_service.rb
+++ app/services/nation_affiliation_service.rb
@@ -1,17 +1,20 @@
 class NationAffiliationService 
 
-  def self.get_url(url, param = nil, perPage = nil)
+  def self.get_url(url, nation = nil, perPage = nil)
     conn = Faraday.new(url: "https://last-airbender-api.herokuapp.com/api/v1/") do |faraday|
       faraday.params[:perPage] = perPage
-      faraday.params[:affiliation] = param unless param == nil
+      faraday.params[:affiliation] = nation.tr("_", " ") unless nation == nil
     end 
-
-  response = conn.get(url)
-  JSON.parse(response.body, symbolize_names: true)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.get_affiliated_characters(affiliation, perPage = "100")
-    get_url("characters", affiliation, perPage)
+  def self.get_affiliated_characters(nation, perPage = "100")
+    get_url("characters", nation, perPage)
   end
+
+  def self.get_character_details(character_id)
+    get_url("characters/#{character_id}")
+  end 
 
 end
