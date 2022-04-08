51463aebafe1b95607ce29606d110d2c13dafb2d
Conor Barthel <conor.barthel@gmail.com>
2022-04-08 10:11:24 -0600

    Feat: Member poro

--- spec/poros/member_spec.rb
+++ spec/poros/member_spec.rb
@@ -3,8 +3,8 @@ RSpec.describe Member do
   it 'builds a nation object when data is passed in' do
     data = {
       id: "1",
-      name: "Fight Club",
-      photo: "member_photo.jpeg",
+      name: "ang",
+      photoUrl: "member_photo.jpeg",
       allies: ["friend"],
       enemies: ["enemy"],
       affiliations: "Navy",
@@ -12,10 +12,11 @@ RSpec.describe Member do
     @nation = Member.new(data)
 
     expect(@nation).to be_an_instance_of(Member)
-    expect(@nation.id).to eq("550")
-    expect(@nation.name).to eq("Fight Club")
-    expect(@nation.summary).to eq("Description here")
+    expect(@nation.id).to eq("1")
+    expect(@nation.name).to eq("ang")
+    expect(@nation.photo).to eq("member_photo.jpeg")
     expect(@nation.allies).to eq(["friend"])
     expect(@nation.enemies).to eq(["enemy"])
     expect(@nation.affiliations).to eq("Navy")
   end
+end
