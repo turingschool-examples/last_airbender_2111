ed4b22728d8222f894fd34c4b7ec41af061a7d00
Tamara Dowis <tamara.dowis@gmail.com>
2022-04-08 11:28:40 -0500

    feat: airbender nation member facade

--- spec/facades/airbender_facade_spec.rb
+++ spec/facades/airbender_facade_spec.rb
@@ -3,7 +3,8 @@ RSpec.describe AirbenderFacade do
   describe 'class methods' do
     it 'will return all members of a nation' do
 
-      nation_members = AirbenderFacade.nation_members(nation)
+      nation_members = AirbenderFacade.nation_members("Fire Nation")
+      expect(nation_members.count).to eq 20
         nation_members.each do |object|
         expect(object).to be_a Member
         expect(object.name).to_not be_nil
