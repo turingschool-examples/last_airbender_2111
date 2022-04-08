bf91c20689b3a6d51033a3f3b9eda1c1f363d208
Sam Iavri <sammi80134@gmail.com>
2022-04-08 11:01:29 -0600

    Query isnt getting passed in correctly. IDK WHAT IM MISSINGAH

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @characters = CharacterFacade.find_characters(params[:q])
+    @characters = CharacterFacade.find_characters(params[:query])
 
     @nation = params[:nation]
   end
