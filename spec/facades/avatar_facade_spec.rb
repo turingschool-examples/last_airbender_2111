8518d9fd9e20907a760be07a26a9875200156ad1
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-04-08 12:58:50 -0400

    feat: refactor to poros

--- spec/facades/avatar_facade_spec.rb
+++ spec/facades/avatar_facade_spec.rb
@@ -6,12 +6,7 @@ RSpec.describe AvatarFacade do
     end
     it 'return an array of hashes' do
       expect(@members).to be_a Array
-      expect(@members.first).to be_a Hash
-      expect(@members.first).to have_key(:_id)
-      expect(@members.first).to have_key(:allies)
-      expect(@members.first).to have_key(:enemies)
-      expect(@members.first).to have_key(:name)
-      expect(@members.first).to have_key(:affiliation)
+      expect(@members.first).to be_a Character
     end
   end
 
