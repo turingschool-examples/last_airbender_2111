dc6cb1700aff2e82af90d6f60a670ac22d230964
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-04-08 10:34:40 -0600

    Facade Update

--- app/facades/airbender_facade.rb
+++ app/facades/airbender_facade.rb
@@ -3,9 +3,9 @@ class AirbenderFacade
     def character_by_affiliation(affiliation)
       response = AirbenderService.characters_with_a_specific_affiliation(affiliation)
 
-      # response.map do |data|
-      #   Airbender.new(data)
-      # end
+      response.map do |data|
+        Airbender.new(data)
+      end
     end
   end
 end
