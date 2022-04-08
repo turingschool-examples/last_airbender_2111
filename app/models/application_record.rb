91d4b9b3019335061aec84aa71843a1a674c2219
Conor Barthel <conor.barthel@gmail.com>
2022-04-08 11:00:45 -0600

    Feat: working on content for search page

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.reformat(nation)
+    split = nation.split('_')
+    reformat = "#{split[0].capitalize}+#{split[1].capitalize}"
+  end
 end
