0e361751c5ee8dc8b9b73b5f8a6caf0bc4d0c101
Conor Barthel <conor.barthel@gmail.com>
2022-04-08 09:53:09 -0600

    Test: feature test for fire nation population

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,3 +1,4 @@
 class SearchController < ApplicationController
-    def index;end
+    def index
+    end
 end
--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -1,1 +1,15 @@
+require 'rails_helper'
 
+RSpec.describe 'Search Page' do
+  before(:each) do
+    visit root_path
+    #save_and_open_page
+    select "Fire Nation"
+    click_on "Search For Members"
+  end
+  VCR.use_cassette('fire_nation') do
+    it "should have a count of total number of people in selected nation" do
+      expect(page).to have_content("Population: 100")
+    end
+  end
+end
