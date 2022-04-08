1446c57b0eb45b846c6593cd747c452493e92256
Joseph W Galvin <jwgalvin@gmail.com>
2022-04-08 10:45:46 -0600

    Test: service tests added.

--- spec/service/last_spec.rb
+++ spec/service/last_spec.rb
@@ -6,7 +6,20 @@ require 'rails_helper'
     it "call on the service" do
       nation = "fire_nation"
       response = LastService.affiliation(nation)
-      binding.pry
+      expect(response).to be_a(Array)
+      expect(response.count).to eq(112)
     end
 
+    it "has attributes" do
+      nation = "fire_nation"
+      response = LastService.affiliation(nation)
+      expect(response[3]).to be_a(Hash)
+      expect(response[3]).to have_key(:name)
+      expect(response[3]).to have_key(:allies)
+      expect(response[3]).to have_key(:enemies)
+      expect(response[3]).to have_key(:photoUrl)
+      expect(response[3]).to have_key(:affiliation)
+      expect(response[3]).to have_key(:_id)
+      #binding.pry
+    end
   end
