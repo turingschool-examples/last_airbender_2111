adf10cbdfe79bd66ceac8a409bf4cfbeeb825c41
Joseph W Galvin <jwgalvin@gmail.com>
2022-04-08 10:11:43 -0600

    Feat: all fire nation showing up

--- app/service/last_service.rb
+++ app/service/last_service.rb
@@ -1,9 +1,18 @@
 class LastService
   class << self
     def affiliation(nation)
-      response =  conn.get("/Api/V1/characters?affiliation=Nation")
+      response = conn.get("/Api/V1/characters?affiliation=Nation&perPage=25&page=1")
+      response2 = conn.get("/Api/V1/characters?affiliation=Nation&perPage=25&page=2")
+      response3 = conn.get("/Api/V1/characters?affiliation=Nation&perPage=25&page=3")
+      response4 = conn.get("/Api/V1/characters?affiliation=Nation&perPage=25&page=4")
+      response5 = conn.get("/Api/V1/characters?affiliation=Nation&perPage=25&page=5")
+      parsed = parse_data(response)
+      parsed2= parse_data(response2)
+      parsed3 = parse_data(response3)
+      parsed4 = parse_data(response4)
+       parsed5 = parse_data(response5)
+      (parsed << parsed2 << parsed3 << parsed4 <<parsed5).flatten
       #binding.pry
-      parse_data(response)
     end
 
     private
