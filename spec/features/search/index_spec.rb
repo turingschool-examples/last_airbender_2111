44e29b0ef5e7c451f499ed6664f80587bfc542ee
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-04-08 11:48:42 -0400

    Feat: add ficture, test and view

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Index' do
+  context 'search returns all members from Fire Nation' do
+    it 'returns members' do
+      VCR.use_cassette('fire_nation') do
+        visit '/'
+        expect(current_path).to eq('/')
+
+
+        select "Fire Nation", :from => :nation
+        click_on "Search For Members"
+
+        expect(page).to have_content("Chan (Fire Nation admiral)")
+      end
+    end
+  end
+end
