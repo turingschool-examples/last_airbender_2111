cc98b6ee805dd315b98c561017ce060011c3f9bf
Jessica Grazulis <jlgrazulis@gmail.com>
2022-04-10 14:54:58 -0600

    Turing PR

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @nation = params[:nation].sub("_", " ").titleize
+    @nation = params[:nation].gsub("_", " ").titleize
     @characters = LastAirbenderFacade.all_nation_characters(@nation)
   end
 end
